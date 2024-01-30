7b1b887c07ba3eb2b9df120301bd0d445c3341ff
Yuhyeon0516 <rladbgus0516@naver.com>
2024-01-30 17:18:37 +0900

    Created randing page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Notfound from "./screens/Notfound";
+import Layout from "./screens/Layout";
+import Home from "./screens/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        errorElement: <Notfound />,
+        element: <Layout />,
+        children: [
+            {
+                path: "",
+                element: <Home />,
+            },
+        ],
+    },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <RouterProvider router={router} />;
 }
 
 export default App;
